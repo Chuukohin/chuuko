e9be8c0cc48b08297d464db2b057975dfd51e49e
nogassname <nanyangimex@hotmail.com>
2022-04-12 12:25:05 +0700

    mainbotton
    bottonหลักที่สีนำ้ตาล

---
+++ frontend/lib/widgets/layout/botton.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Mainbotton extends StatelessWidget {
+  String title;
+  double height;
+  double width;
+  Mainbotton(this.title, this.height, this.width);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+            borderRadius: new BorderRadius.all(Radius.circular(39)),
+            color: Color(0xFFB89476)),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.white,
+            ),
+          ),
+        ));
+  }
+}
