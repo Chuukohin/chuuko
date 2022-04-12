38eeb8cea09304b734041d8ebe9f2a2eeacb1466
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2022-04-13 01:33:35 +0700

    add: validator

--- backend/endpoints/account/register/types.go
+++ backend/endpoints/account/register/types.go
@@ -1,10 +1,10 @@
 package register
 
 type request struct {
-	Firstname       *string `json:"firstname"`
-	Lastname        *string `json:"lastname"`
-	Email           *string `json:"email"`
-	Password        *string `json:"password"`
+	Firstname       *string `json:"firstname" validate:"required"`
+	Lastname        *string `json:"lastname" validate:"required"`
+	Email           *string `json:"email" validate:"required,email,max=255"`
+	Password        *string `json:"password" validate:"required,gte=8"`
 	ConfirmPassword *string `json:"confirm_password"`
 }
 
--- backend/utils/crypto/bcrypt.go
+++ backend/utils/crypto/bcrypt.go
@@ -3,7 +3,7 @@ package crypto
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	var hashedPassword, err = bcrypt.GenerateFromPassword([]byte(password), bcrypt.MaxCost)
+	var hashedPassword, err = bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(hashedPassword), err
 }
 
