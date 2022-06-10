d36f8987be4312b7ac477ff1e375bf63968aabf6
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2022-06-11 01:29:01 +0700

    fix: product.detail.post

--- backend/endpoints/product/handler_detail_post.go
+++ backend/endpoints/product/handler_detail_post.go
@@ -5,8 +5,15 @@ import (
 	"chuukohin/models"
 	"chuukohin/types/fiber/jwt_claim"
 	"chuukohin/types/responder"
+	"chuukohin/utils/storage"
+	"chuukohin/utils/text"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	"image"
+	"image/jpeg"
+	"os"
+	"path"
+	"strconv"
 )
 
 // ProductDetailPostHandler
@@ -25,27 +32,30 @@ func ProductDetailPostHandler(c *fiber.Ctx) error {
 	token := c.Locals("user").(*jwt.Token)
 	claims := token.Claims.(*jwt_claim.UserClaim)
 
-	// * Parse Body
-	body := new(productDetailRequest)
-	if err := c.BodyParser(&body); err != nil {
-		return &responder.GenericError{
-			Message: "Unable to parse body",
-			Err:     err,
-		}
-	}
+	// * Parse body from multipart
+	productName := c.FormValue("name")
+	productDescription := c.FormValue("description")
+	productBrand := c.FormValue("brand")
+	price, _ := strconv.ParseUint(c.FormValue("price"), 10, 64)
+	productPrice := price
+	categoryId, _ := strconv.ParseUint(c.FormValue("category_id"), 10, 64)
+	productCategory := categoryId
 
-	defaultId := uint64(1)
-	selling := "selling"
+	body := &productDetailRequest{
+		Name:        &productName,
+		Description: &productDescription,
+		Brand:       &productBrand,
+		Price:       &productPrice,
+		CategoryId:  &productCategory,
+	}
 
 	product := &models.Product{
 		SellerId:    claims.SellerId,
-		PictureId:   &defaultId,
-		CategoryId:  body.Category,
 		Name:        body.Name,
 		Description: body.Description,
 		Brand:       body.Brand,
 		Price:       body.Price,
-		Status:      &selling,
+		CategoryId:  body.CategoryId,
 	}
 
 	// * Create product
@@ -56,5 +66,95 @@ func ProductDetailPostHandler(c *fiber.Ctx) error {
 		}
 	}
 
+	// * Parse multipart file parameter
+	fileHeader, err := c.FormFile("image")
+	if err != nil {
+		if err := deleteProduct(product.Id); err != nil {
+			return err
+		}
+		return &responder.GenericError{
+			Message: "Unable to parse image file",
+			Err:     err,
+		}
+	}
+
+	// * Open multipart to file
+	file, err := fileHeader.Open()
+	if err != nil {
+		if err := deleteProduct(product.Id); err != nil {
+			return err
+		}
+		return &responder.GenericError{
+			Message: "Unable to parse image file",
+			Err:     err,
+		}
+	}
+
+	// * Decode image
+	img, _, err := image.Decode(file)
+	if err != nil {
+		if err := deleteProduct(product.Id); err != nil {
+			return err
+		}
+		return &responder.GenericError{
+			Message: "Unable to decode image",
+			Err:     err,
+		}
+	}
+
+	// * Assign file path
+	filePath := path.Join(storage.ProductsDir, strconv.FormatUint(*product.Id, 10))
+	fileSalt := *text.GenerateString(text.GenerateStringSet.Num, 6)
+
+	// * Save image to file
+	savingFile, err := os.Create(filePath + "." + fileSalt + ".jpeg")
+	if err != nil {
+		if err := deleteProduct(product.Id); err != nil {
+			return err
+		}
+		return &responder.GenericError{
+			Message: "Unable to create an image file",
+			Err:     err,
+		}
+	}
+	defer savingFile.Close()
+
+	if err := jpeg.Encode(savingFile, img, nil); err != nil {
+		if err := deleteProduct(product.Id); err != nil {
+			return err
+		}
+		return &responder.GenericError{
+			Message: "Unable to save an image file",
+			Err:     err,
+		}
+	}
+
+	// * Create shop_pic
+	location := "/static/products/" + strconv.FormatUint(*product.Id, 10) + "." + fileSalt + ".jpeg"
+	productLocation := location
+	productPicture := &models.Picture{
+		PictureUrl: &productLocation,
+	}
+
+	if result := database.Gorm.Create(&productPicture); result.Error != nil {
+		if err := deleteProduct(product.Id); err != nil {
+			return err
+		}
+		return &responder.GenericError{
+			Message: "Unable to add product picture",
+			Err:     result.Error,
+		}
+	}
+
 	return c.JSON(responder.NewInfoResponse("Add product successfully"))
+}
+
+func deleteProduct(id *uint64) error {
+	if result := database.Gorm.Delete(&models.Product{}, id); result.Error != nil {
+		return &responder.GenericError{
+			Message: "Unable to delete the product",
+			Err:     result.Error,
+		}
+	}
+	return nil
 }
