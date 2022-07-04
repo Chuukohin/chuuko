5304ed6de9d9c2e8f82898b0ef43981bc0d51aa1
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2022-07-05 03:54:01 +0700

    pref: me.order_status.received.patch

--- backend/endpoints/me/order_status/handler_order_status_received.go
+++ backend/endpoints/me/order_status/handler_order_status_received.go
@@ -3,8 +3,11 @@ package order_status
 import (
 	"chuukohin/database"
 	"chuukohin/models"
+	"chuukohin/types/model_shop"
 	"chuukohin/types/responder"
+	"encoding/json"
 	"github.com/gofiber/fiber/v2"
+	"time"
 )
 
 // OrderStatusReceivedHandler
@@ -28,8 +31,52 @@ func OrderStatusReceivedHandler(c *fiber.Ctx) error {
 		}
 	}
 
+	var orderDetail *models.OrderDetail
+
+	if result := database.Gorm.First(&orderDetail, "order_id = ?", *body.OrderId); result.Error != nil {
+		return &responder.GenericError{
+			Message: "eeee",
+		}
+	}
+
+	// * Convert type
+	m := string(*orderDetail.Details)
+	orderDetailDetails := new(model_shop.OrderDetail)
+	err := json.Unmarshal([]byte(m), orderDetailDetails)
+	now := time.Now().UTC().Format("2006-01-02 15:04:05.0000000 +0000")
+	nowTime, _ := time.Parse("2006-01-02 15:04:05.0000000 +0000", now)
+	if err != nil {
+		return &responder.GenericError{
+			Message: "Unable to unmarshal json",
+			Err:     err,
+		}
+	}
+
+	success := "Success"
+	receivedTrue := true
+	received := &model_shop.DeliveryDetail{
+		Status:      &success,
+		Description: nil,
+		Time:        &nowTime,
+	}
+
+	var deliveryDetails []*model_shop.DeliveryDetail
+	for _, status := range orderDetailDetails.Status {
+		deliveryDetails = append(deliveryDetails, status)
+	}
+
+	orderDetail.Received = &receivedTrue
+	orderDetail.Status = &success
+	deliveryDetails = append(deliveryDetails, received)
+
+	details := &model_shop.OrderDetail{
+		ProductId:      orderDetailDetails.ProductId,
+		TrackingNumber: orderDetailDetails.TrackingNumber,
+		Status:         deliveryDetails,
+	}
+
 	// * Update order status
-	if result := database.Gorm.Model(new(models.OrderDetail)).Where("order_id = ?", *body.OrderId).Update("received", "true"); result.Error != nil {
+	if result := database.Gorm.Model(new(models.OrderDetail)).Where("order_id = ?", *body.OrderId).Update("received", "true").Update("details", details); result.Error != nil {
 		return &responder.GenericError{
 			Message: "Order not found",
 			Err:     result.Error,
